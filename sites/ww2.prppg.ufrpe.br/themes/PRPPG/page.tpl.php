6edadc469b62d9029bba0aadacf10deda9a84de9
Rodolfo Ferreira <rodolfoandreferreira@gmail.com>
2015-03-13 14:05:07 -0300

    Atualização final de layout e conteúdo
    Todas atualizações realizadas até o momento. *Falta o banco de dados*

--- sites/ww2.prppg.ufrpe.br/themes/PRPPG/page.tpl.php
+++ sites/ww2.prppg.ufrpe.br/themes/PRPPG/page.tpl.php
@@ -71,15 +71,16 @@
 ?>
 
 	<div id="page-wrapper"><div id="page">
-		<div id="header-wrap" class="wrap">
-			<div id="header" class="section clearfix content">
-				<div id="barra-brasil">
-					<a href="http://www.acessoainformacao.gov.br/" style="position: relative; left:60%;"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/Acesso.png'; ?>"/></a>
-					<a href="http://ufrpe.br" style="position: relative; left:60%"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/BRASIL.png'; ?>" /></a>
+<div id="barra-brasil">
+					<a href="http://www.acessoainformacao.gov.br/" style="position: relative; left:55%;"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/Acesso.png'; ?>"/></a>
+					<a href="http://ufrpe.br" style="position: relative; left:55%"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/BRASIL.png'; ?>" /></a>
 						<div id="language">
 							<?php print render($page['language']);?>
 						</div>
 				</div>
+		<div id="header-wrap" class="wrap">
+			<div id="header" class="section clearfix content">
+				
 				<div id="Head">
 					<div id="divBrasao">
 						<a href="http://ufrpe.br" title="UFRPE" >
@@ -92,7 +93,7 @@
 							
 					</div>
 					<a href="<?php print $front_page; ?>" title="<?php print t('Inicio'); ?>" rel="home">
-					<div id="divTitulo" style="width: 500px; height: 140px"><center id="Titulo"><p class="Titulo"> <?php print $site_name; ?> </p> <p class="Slogan"> <?php print $site_slogan; ?> </p></center>
+					<div id="divTitulo" style="width: 500px; height: 140px;"><center id="Titulo"><p class="Titulo"> <?php print $site_name; ?> </p> <p class="Slogan"> <?php print $site_slogan; ?> </p></center>
 					</div></a>
         
 					<div id="divBusca" style="text-align:center">
@@ -159,22 +160,101 @@
 		<div id="footer-wrap" class="wrap">
 			<div id="footer" class="section clearfix content">
 				<div id="Outros">
-            	<h2>Outros Sites</h2>
-                <a href="http://www.periodicos.capes.gov.br"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/periodicos.png'; ?>" alt="Periodicos Capes"/></a>
-                <a href="http://www.cnpq.br/"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/CNPq.png'; ?>" alt="CNPq"/></a>
-                <a href="http://www.facepe.br"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/FACEPE.png'; ?>" alt="FACEPE"/></a> 
-                <a href="http://www.finep.gov.br"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/FINEP.png'; ?>" alt="FINEP"/></a>
-                <a href="http://www.sudene.gov.br/"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/Sudene.png'; ?>" alt="Sudene"/></a>
-                <a href="http://www.capes.gov.br/"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/Capes.png'; ?>" alt="CAPES"/></a>	
-                <span style="padding-left:130px;"><a href="http://www.bnb.gov.br/Content/Aplicacao/Grupo_Principal/Home/Conteudo/PortalBN.asp"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/Banco.png'; ?>" alt="Bando do Nordeste"/></a></span>
+		<?php if (!$logged_in): ?>
+					<div id="login">
+						  <div class="region region-footer">
+    		<div id="block-user-login" class="block block-user">
+    		<h2 style="
+    				margin-top: 0px;
+    				margin-bottom: 0px;
+			">Login do usuário</h2>
+
+  
+  			<div class="content">
+  					 <form action="/br/node?destination=node" method="post" id="user-login-form" accept-charset="UTF-8"><div><div class="form-item form-type-textfield form-item-name">
+ 					 <label for="edit-name">Usuário <span class="form-required" title="Este campo é obrigatório.">*</span></label>
+ 				<input type="text" id="edit-name" name="name" value="" size="15" maxlength="60" class="form-text required">
+			</div>
+		<div class="form-item form-type-password form-item-pass">
+  		<label for="edit-pass">Senha <span class="form-required" title="Este campo é obrigatório.">*</span></label>
+ 		<input type="password" id="edit-pass" name="pass" size="15" maxlength="128" class="form-text required">
+		</div>
+		<div class="item-list"><ul><li class="first last"><a href="/br/user/password" title="Solicitar uma nova senha por email.">Recuperar senha</a></li>
+		</ul></div><input type="hidden" name="form_build_id" value="form-7pfYKr3g_7EMzKRwxnekRKoFK_KajROPMEevz2_Aorw">
+		<input type="hidden" name="form_id" value="user_login_block">
+		<div class="form-actions form-wrapper" id="edit-actions--2"><input type="submit" id="edit-submit--2" name="op" value="Entrar" class="form-submit"></div></div></form>  </div>
+		</div>	
+  	</div>
+					
+            		<h2 style="
+   	width: 300px;
+		">Sites</h2>
+			<a href="http://www.capes.gov.br/" style="
+    	padding-left: 70PX;
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/Capes.png" alt="CAPES"></a>
+		<a href="http://www.cnpq.br/" style="
+    padding-left: 100PX;
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/CNPq.png" alt="CNPq"></a>
+		<a href="http://www.facepe.br" style="
+    padding-left: 100px;
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/FACEPE.png" alt="FACEPE"></a>
+                <a href="http://www.periodicos.capes.gov.br" style="
+    padding-left: 50px;
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/periodicos.png" alt="Periodicos Capes"></a>
+                
+                 
+                <a href="http://www.finep.gov.br" style="
+    padding-left: 20px;
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/FINEP.png" alt="FINEP"></a>
+                <a href="http://www.sudene.gov.br/" style="
+    padding-left: 30px;
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/Sudene.png" alt="Sudene"></a>
+                	
+                <span style="padding-left: 20px;"><a href="http://www.bnb.gov.br/Content/Aplicacao/Grupo_Principal/Home/Conteudo/PortalBN.asp"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/Banco.png" alt="Bando do Nordeste"></a></span>
+		
             </div>
-			<div id="NTI">
-				<p>© Copyright 2012</p>
-				<p>Universidade Federal Rural de Pernambuco</p>
-				<a href="http://www.nti.ufrpe.br/‎"><img src="<?php print base_path() . path_to_theme() . '/' . 'images/NTI.png'; ?>" alt="Núcleo de Tecnologia da Informação UFRPE"/></a>
-			</div>
-			<div id="login">
-				<?php print render($page['footer']); ?>
-			</div>
+<?php endif; ?>	</div>
+<?php if ($logged_in): ?>
+
+<h2 style="
+   	width: 300px;
+		">Sites</h2>
+			<a href="http://www.capes.gov.br/" style="
+
+    	padding-left: 50PX;
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/Capes.png" alt="CAPES"></a>
+		<a href="http://www.cnpq.br/" style="
+    padding-left:50PX;
+
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/CNPq.png" alt="CNPq"></a>
+		<a href="http://www.facepe.br" style="
+    padding-left: 50px;
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/FACEPE.png" alt="FACEPE"></a>
+                <a href="http://www.periodicos.capes.gov.br" style="
+    padding-left: 50px;
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/periodicos.png" alt="Periodicos Capes"></a>
+                
+                 
+                <a href="http://www.finep.gov.br" style="
+    padding-left: 20px;
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/FINEP.png" alt="FINEP"></a>
+                <a href="http://www.sudene.gov.br/" style="
+    padding-left: 30px;
+"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/Sudene.png" alt="Sudene"></a>
+                	
+                <span style="padding-left: 20px;"><a href="http://www.bnb.gov.br/Content/Aplicacao/Grupo_Principal/Home/Conteudo/PortalBN.asp"><img src="/sites/ww2.prppg.ufrpe.br/themes/PRPPG/images/Banco.png" alt="Bando do Nordeste"></a></span>
+
+<?php endif; ?>	
+<div id="block-block-2" class="block block-block" style="margin-top: 20px;">
+
+      
+    <p class="rtecenter" align="center" style="margin-bottom: 2px; font-size: 12px; ">Universidade Federal Rural de Pernambuco - UFRPE</p>
+<p class="rtecenter" align="center" style="margin-bottom: 2px; font-size: 12px; ">Rua Dom Manoel de Medeiros, s/n, Dois Irmãos - CEP: 52171-900 - Recife/PE</p>
+  
+</div>
+
+</div>
+
+			
 			</div>
 		</div>
